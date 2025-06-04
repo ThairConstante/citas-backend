f626a93b0bcd52e8c2377ee8cae25683ba88c2ca
ThairConstante <thairconstante123@gmail.com>
2025-06-04 14:00:28 -0500

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root@127.0.0.1:3306/db_citas_medicas"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
